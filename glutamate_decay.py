57c8050f20e7eee852b360cf59e9207f4f567f6f
antony <antonykiran10@gmail.com>
2021-12-20 15:24:56 +0530

    prilimniary model without gliotrasmitter release

---
+++ glutamate_decay.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+
+def model(z, t):
+    Glu = z
+    k_Glu = 10
+    dGludt = (-Glu * k_Glu)
+    return dGludt
+
+
+initial_vals = [200]
+
+time_points = np.linspace(0, 10, int(10 / 0.001))
+
+result = odeint(model, initial_vals, time_points)
+
+plt.plot(time_points,result, label="Glu")
+plt.grid()
+plt.legend()
+plt.show()
+
