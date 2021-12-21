dc6e2aaa094b2dfc299a503a9ffa9b53822562a5
antony <antonykiran10@gmail.com>
2021-12-21 14:01:03 +0530

    signal generating test codes

---
+++ square wave.py
@@ -0,0 +1,13 @@
+from scipy import signal
+
+import matplotlib.pyplot as plot
+
+import numpy as np
+
+# Sampling rate 1000 hz / second
+
+t = np.linspace(0, 200, 200)
+
+plot.plot(t, signal.square(2 * np.pi *5* t))
+plot.grid()
+plot.show()
---
+++ train_generator.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = []
+
+for i in range(0,100):
+    if i%2 == 0:
+        a.append(1)
+    else:
+        a.append(0)
